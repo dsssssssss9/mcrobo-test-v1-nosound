a7a42af6343c5c92a7290a58dbfa72490976bb54
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-10-18 15:41:08 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,104 @@
+function show_omg () {
+    for (let index2 = 0; index2 <= omg.length - 1; index2++) {
+        strip.setPixelColor(omg[index2], neopixel.colors(NeoPixelColors.Violet))
+        strip.show()
+    }
+    basic.pause(1000)
+    strip.clear()
+    strip.show()
+}
+function show_frown () {
+    for (let index22 = 0; index22 <= frown.length - 1; index22++) {
+        strip.setPixelColor(frown[index22], neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    }
+    basic.pause(500)
+    strip.clear()
+    strip.show()
+}
+function show_eyes () {
+    for (let index = 0; index <= botheyes.length - 1; index++) {
+        strip.setPixelColor(botheyes[index], neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    }
+    basic.pause(100)
+    strip.clear()
+    strip.show()
+}
+input.onButtonPressed(Button.A, function () {
+    show_smile()
+})
+function show_smile () {
+    for (let index23 = 0; index23 <= smile.length - 1; index23++) {
+        strip.setPixelColor(smile[index23], neopixel.colors(NeoPixelColors.Violet))
+        strip.show()
+    }
+    basic.pause(500)
+    strip.clear()
+    strip.show()
+}
+function init () {
+    nose = 14
+    lefteye = 15
+    righteye = 16
+    botheyes = [15, 16]
+    smile = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5
+    ]
+    frown = [
+    0,
+    5,
+    6,
+    7,
+    8,
+    9
+    ]
+    omg = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+    ]
+}
+input.onButtonPressed(Button.AB, function () {
+    show_omg()
+})
+input.onButtonPressed(Button.B, function () {
+    strip.setBrightness(255)
+    for (let index = 0; index < 6; index++) {
+        show_eyes()
+        basic.pause(100)
+    }
+    strip.setBrightness(32)
+})
+function show_nose () {
+    strip.setPixelColor(nose, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(200)
+    strip.clear()
+    strip.show()
+}
+let righteye = 0
+let lefteye = 0
+let nose = 0
+let smile: number[] = []
+let botheyes: number[] = []
+let frown: number[] = []
+let omg: number[] = []
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 17, NeoPixelMode.RGB)
+strip.setBrightness(32)
+strip.clear()
+strip.show()
+init()
